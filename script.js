5cc261a7272698e457c539ce3457c3e89ca7b821
Lucas R. de Souza <LucasRSouza2000@outlook.com>
2022-10-01 22:50:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,145 @@
 'use script';
 
 const btnS = document.querySelectorAll('.buttons');
-const taskOutputDiv = document.querySelector('#taskOutput');
-const taskInputText = document.querySelector('#taskInputText');
+const taskOutputDiv = document.querySelectorAll('.taskOutput');
 const addTaskBtn = document.querySelector('#addTask');
 const slideRightBtn = document.querySelector('#slideRight');
 const slideLeftBtn = document.querySelector('#slideLeft');
 const closeDiv = document.querySelector('.closeDiv');
 
-const styleValue = document.querySelector('.task_');
-const styleValue_2 = document.querySelector('#task');
+/////////////////////////////////////////////
+const clear = document.querySelector('#clear');
+const list = document.getElementById('list');
+const input = document.getElementById('input');
+const date_paragraph = document.querySelector('#counter')
+/////////////////////////////////////////////
+// 2 - Classes Names
+
+const CHECK = 'fa-check-circle';
+const UNCHECK = 'fa-circle-thin';
+const LINE_THROUGH = 'lineThrough';
+
+// 5 - variables
+
+//let LIST = [], id = 0;
+let LIST, id;
+
+// 9 - now we are gonna deal with the local storage (get item from local storage)
+
+// 10 - get item from local storage
+
+let data = localStorage.getItem('TODO');
+
+// 11 - check if data is not empty 
+
+if (data) {
+    // if theres data in the locaStorage
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+
+} else {
+    // if theres no data in the localStorage 
+    // lets say that's the first time the user uses the application
+    LIST = [];
+    id = 0;
+}
+
+// 12 - Load items to the user's interface
+
+function loadList(array) {
+    array.forEach(item => {
+        addToDo(item.name, item.id, item.done, item.trash)
+    })
+}
+
+// 13 - clear the local storage
+
+clear.addEventListener('click', function() {
+    localStorage.clear();
+    location.reload();
+})
+
+
+//localStorage.setItem('TODO', JSON.stringify(LIST)); // This code must be added where the LIST array is updated
+
+
+// 3 - Add To-Do Function
+
+function addToDo(toDo, id, done, trash) {
+    const position = 'beforeend';
+
+    if (trash) { return; } // Check if the item isnt in the trash. if its in the trash we will return it (it will prevent the code below to run)
+    
+    const DONE = done ? CHECK : UNCHECK; // check if the item is done (if it is then it will add the check class and if its not it will add the uncheck class)
+    const LINE = done ? LINE_THROUGH : ''; // Same thing for the LINE_THROUGH
+
+    const item = `
+    <li id="item">
+    <i class="fa ${DONE} co" job="complete" id="${id}" ></i>
+    <p class="text ${LINE}">${toDo}</p>
+    <i class="fa fa-trash-o de clear" job="delete" id="${id}">
+    </i>`;
+
+    list.insertAdjacentHTML(position, item);
+}
+
+// 4 - Add an Item to the list user the enter key
+
+addTaskBtn.addEventListener('click', function(event) {
+    //if (event.key == 'Enter') {
+        const toDo = input.value; // Get the value of the input
+        if (toDo) { // Check if toDo is not empty
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false,
+            }) // we are gonna push the item // done and trash will be false cuz the user just entered a new ToDo Item
+
+            id++; // Increase the id by 1
+
+            localStorage.setItem('TODO', JSON.stringify(LIST)); // This code must be added where the LIST array is updated
+        } 
+
+        input.value = '';
+    //}
+})
+
+/*document.addEventListener('keydown', function(event) {
+    event.preventDefault()
+    if (event.key == 'Enter') {
+        const toDo = input.value; // Get the value of the input
+        if (toDo) { // Check if toDo is not empty
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false,
+            }) // we are gonna push the item // done and trash will be false cuz the user just entered a new ToDo Item
+
+            id++; // Increase the id by 1
+
+            localStorage.setItem('TODO', JSON.stringify(LIST)); // This code must be added where the LIST array is updated
+        } 
+
+        input.value = '';
+    }
+})*/
+
+//addToDo('Estudar JavaScript', 1, true, false); // Check if the check/uncheck and line through are working
+// if the trash is true then the item will disappear (it means its removed)
+
+let itemDiv = document.querySelector('#remainingItems');
 
 slideRightBtn.addEventListener('click', function() {
 
     if (addTaskBtn.style.display = 'none') {
-        taskInputText.style.width = '89%';
+        input.style.width = '89%';
         addTaskBtn.style.display = 'block';
         slideLeftBtn.style.display = 'block';
         slideRightBtn.style.display = 'none';
@@ -24,225 +149,56 @@ slideRightBtn.addEventListener('click', function() {
 
 slideLeftBtn.addEventListener('click', function() {
     if (addTaskBtn.style.display = 'block') {
-        taskInputText.style.width = 0;
+        input.style.width = 0;
         addTaskBtn.style.display = 'none';
         slideLeftBtn.style.display = 'none';
         slideRightBtn.style.display = 'block';
     }
 })
 
-/*btnS.forEach(b => {
-    b.addEventListener('mouseover', function() {
-        b.style.color = '#5b9fe3';
-        b.style.fontWeight = 'bold';
-    })
+// 6 - Complete To do
 
-    b.addEventListener('mouseout', function() {
-        b.style.color = '';
-        b.style.fontWeight = '';
-    })
-})*/
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH);
 
+    // Update our list array
 
+    // Set the done to false if it was true and true if it was false
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
 
-let task_div;
-let task_input;
-let pseudoDiv;
-let identifier;
-let taskCounter = [];
-let counterParagraph = document.querySelector('#counter');
+// 7 - Remove To do
 
+// remove th item from the user interface
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
 
-const createTask = function() {
+// 8 - add an event lsitener to the list to target our items created dynamically
 
-    const taskValue = taskInputText.value;
+list.addEventListener('click', function(event) {
+    const element = event.target; // it will return the clicked element inside the list
+    //const elementJob = element.attributes.job.value; // complete or delete
+    const elementJob = element.getAttribute('job');
 
-    // Div
-    task_div = document.createElement('div');
-    task_div.setAttribute('id', 'task');
-    task_div.setAttribute('class', 'task');
-    task_div.setAttribute('class', 'closeDiv');
+    if (elementJob == 'complete') {
+        completeToDo(element)
+    } else if (elementJob == 'delete') {
+        removeToDo(element)
+    }
 
-    // Task
-    const task_text = document.createElement('p');
-    task_text.setAttribute('id', 'task_')
-    task_text.innerHTML = taskValue;
     
-    // Form & Input
-    const task_form = document.createElement('form');
-    task_form.setAttribute('id', 'form');
+    localStorage.setItem('TODO', JSON.stringify(LIST)); // This code must be added where the LIST array is updated
+}) 
 
-    // Checkbox Input
-    task_input = document.createElement('input');
-    task_input.setAttribute('type', 'checkbox');
-    task_input.setAttribute('id', 'checkbox');
+// Date
 
-    // Close
+const options = {weekday: 'long', month: 'short', day: 'numeric'};
+const today = new Date();
 
-    pseudoDiv = document.createElement('div');
-    pseudoDiv.setAttribute('id', 'closeDiv');
-    pseudoDiv.setAttribute('class', 'closeDiv');
+date_paragraph.innerHTML = today.toLocaleString('pt-BR', options);
 
-    // Insert elements inside div
-
-    task_form.appendChild(task_input);
-
-    task_div.appendChild(task_form);
-
-    task_div.appendChild(task_text);
-
-    task_div.appendChild(pseudoDiv);
-
-    // Add Div With Task Included Inside HTML 
-
-    const addTask = taskOutputDiv.appendChild(task_div);
-
-    taskInputText.value = '';
-
-    taskCounter.push(addTask);
-
-    //counter.innerHTML = taskCounter.length;
-
-    console.log(taskCounter.length)
-
-    if (taskCounter.length > 1) {
-        para.innerHTML = `${counterParagraph.innerHTML = taskCounter.length} ITENS RESTANTE`;
-    } else {
-        para.innerHTML = `${counterParagraph.innerHTML = taskCounter.length} ITEM RESTANTE`;
-    }
-
-};
-
-console.log(taskCounter)
-
-let para = document.querySelector('#paragraph_1');
-
-
-addTaskBtn.addEventListener('click', createTask);
-
-const closeDivBtn = document.querySelectorAll('#closeDiv');
-const checkedBtn = document.querySelector('#checkbox');
-
-const popUp = document.querySelector('.popUp');
-const popUp_2 = document.querySelector('.popUp_2');
-
-taskOutputDiv.addEventListener('click', function(e) {
-    const checkbox_element = e.target.getAttribute('id');
-    const removeDivButton = e.target.getAttribute('id');
-    
-    const el_1 = e.target.closest(`#task`);
-    
-    const el_2 = e.target.parentElement.children;
-    console.log(el_2)
-    
-
-    // Remover Tarefas
-
-    taskCounter.pop();
-
-    //console.log(taskCounter.length)
-
-    if (checkbox_element === 'checkbox') {
-        popUp.style.opacity = '100';
-
-        setTimeout(removePopUp, 3000);
-
-        function removePopUp() {
-            popUp.style.opacity = 0;
-        }
-    }
-
-    if (removeDivButton === 'closeDiv') {
-        taskOutputDiv.removeChild(el_1);
-
-        if (taskCounter.length > 1 - 1) {
-            para.innerHTML = `${counterParagraph.innerHTML = taskCounter.length} ITENS RESTANTE`;
-        } else {
-            para.innerHTML = `${counterParagraph.innerHTML = taskCounter.length} ITEM RESTANTE`;
-        }
-
-        popUp_2.style.opacity = '100';
-        setTimeout(removedTaskPopUp, 3000);
-
-        function removedTaskPopUp() {
-            popUp_2.style.opacity = 0;
-        }
-    }
-
-    // Dar Checkup nas Tarefas
-
-    /*if (el_2_checkbox.checked = true) {
-
-        popUp.style.opacity = '100';
-
-        setTimeout(removePopUp, 3000);
-
-        function removePopUp() {
-            popUp.style.opacity = 0;
-        }
-    }*/
-
-const removeTaskBtn = document.querySelector('.removeCompletedTask');
-
-removeTaskBtn.addEventListener('click', function(e) {
-
-    if (el_2.checked = true) {
-        taskOutputDiv.removeChild(el_1);
-
-        popUp_2.style.opacity = '100';
-        setTimeout(removedTaskPopUp, 3000);
-
-        function removedTaskPopUp() {
-            popUp_2.style.opacity = 0;
-        }
-    }
-
-    //taskCounter.pop();
-
-    if (taskCounter.length > 1 - 1) {
-        para.innerHTML = `${counterParagraph.innerHTML = taskCounter.length} ITENS RESTANTE`;
-    } else {
-        para.innerHTML = `${counterParagraph.innerHTML = taskCounter.length} ITEM RESTANTE`;
-    }
-
-});
-})
-
-// Slider
-
-const rightBtn = document.querySelector('#btn_right');
-const leftBtn = document.querySelector('#btn_left');
-const slides = document.querySelectorAll('.slide');
-const slider = document.querySelector('#gallery_quotes');
-
-const maxSlides = slides.length;
-let curSlide = 0;
-
-slides.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * i}%)`;
-})
-
-rightBtn.addEventListener('click', function() {
-    
-    if (curSlide === maxSlides - 1) {
-        curSlide = 0;
-    } else {
-        curSlide++;
-    }
-
-    slides.forEach((s, i) => {
-        s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
-    })
-})
-
-leftBtn.addEventListener('click', function() {
-    if (curSlide === 0) {
-        curSlide = maxSlides - 1;
-    } else {
-        curSlide--;
-    }
-
-    slides.forEach((s, i) => {
-        s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
-    })
-})
+// Updated Code
